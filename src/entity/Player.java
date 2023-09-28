e46591510c71a17c2cec0c6824774c33752bd03a
ljw12334 <ljw12334@g.shingu.ac.kr>
2023-09-29 01:11:51 +0900

    카메라 추가, 맵 랜더링 방식 변경

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -12,17 +12,27 @@ public class Player extends Entity {
     GamePanel gp;
     KeyHandler keyH;
 
+    //실제로 플레이어가 화면에 그려지게 되는 좌표
+    public final int screenX;
+    public final int screenY;
+
     public Player(GamePanel gp, KeyHandler keyH) {
         this.gp = gp;
         this.keyH = keyH;
+
+        // 스프라이트 좌상단이 (0, 0) 이므로, 플레이어를 정확히 화면 중앙에 표시하기 위해서 타일 반개 만큼의 크기를 빼준다
+        screenX = (gp.screenWidth / 2) - (gp.tileSize / 2);
+        screenY = (gp.screenHeight / 2) - (gp.tileSize / 2);
 
         setDefaultValues();
         getPlayerImage();
     }
     public void setDefaultValues() {
-        x = 100;
-        y = 100;
-        speed = 4;
+        // 게임 상에서의 플레이어의 좌표
+        worldX = gp.tileSize * 23;
+        worldY = gp.tileSize * 21;
+
+        speed = 3;
         direction = "down";
     }
     public void getPlayerImage() {
@@ -45,16 +55,16 @@ public class Player extends Entity {
         if (keyH.upPressed == true || keyH.downPressed == true || keyH.leftPressed == true || keyH.rightPressed == true) {
             if (keyH.upPressed == true) {
                 direction = "up";
-                y -= speed;
+                worldY -= speed;
             } else if (keyH.downPressed == true) {
                 direction = "down";
-                y += speed;
+                worldY += speed;
             } else if (keyH.leftPressed == true) {
                 direction = "left";
-                x -= speed;
+                worldX -= speed;
             } else if (keyH.rightPressed == true) {
                 direction = "right";
-                x += speed;
+                worldX += speed;
             }
 
             spriteCounter++;
@@ -108,6 +118,6 @@ public class Player extends Entity {
                 }
                 break;
         }
-        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
     }
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -19,12 +19,19 @@ public class GamePanel extends JPanel implements Runnable {
     public final int screenWidth = tileSize * maxScreenCol; // 가로 768픽셀
     public final int screenHeight = tileSize * maxScreenRow; // 세로 576픽셀
 
+    // 월드 설정
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int worldWidth = tileSize * maxWorldCol;
+    public final int worldHeight = tileSize * maxScreenRow;
+
+
     int FPS = 60;
 
     TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
-    Player player = new Player(this, keyH);
+    public Player player = new Player(this, keyH);
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
